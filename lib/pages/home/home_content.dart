e6170bfe1985946b80bf57ffbc88f99ed4534948
kdudley9 <98002234+kdudley9@users.noreply.github.com>
2024-04-23 00:19:19 -0400

    Add logout button to feed

--- lib/pages/home/home_content.dart
+++ lib/pages/home/home_content.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/pages/registration/login_page.dart';
 import 'package:social_media_app/services/authentication_service.dart';
 import 'package:social_media_app/themes/app_colors.dart';
 import 'package:social_media_app/components/post.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class HomeContent extends StatelessWidget {
+class HomeContent extends StatefulWidget {
   const HomeContent({super.key});
+
+  @override
+  State<HomeContent> createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent> {
+  final AuthenticationService _authenticationService = AuthenticationService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Feed'),
+        centerTitle: true,
         backgroundColor: AppColors.minglRed,
         foregroundColor: AppColors.minglWhite,
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await _authenticationService.logout();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            },
+            icon: const Icon(Icons.logout_outlined),
+          )
+        ],
       ),
       body: Column(
         children: [
@@ -33,13 +54,15 @@ class HomeContent extends StatelessWidget {
             child: StreamBuilder(
               stream: FirebaseFirestore.instance
                   .collection("User Posts")
-                  .orderBy("Timestamp", descending: true) // Show newest posts first
+                  .orderBy("Timestamp",
+                      descending: true) // Show newest posts first
                   .snapshots(),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   // Building a list of posts
                   return ListView.builder(
-                    itemCount: snapshot.data!.docs.length, // Define number of items
+                    itemCount:
+                        snapshot.data!.docs.length, // Define number of items
                     itemBuilder: (context, index) {
                       // Get the individual post
                       final post = snapshot.data!.docs[index];
@@ -55,7 +78,8 @@ class HomeContent extends StatelessWidget {
                   );
                 } else {
                   return const Center(
-                    child: CircularProgressIndicator(), // Loading indicator while fetching data
+                    child:
+                        CircularProgressIndicator(), // Loading indicator while fetching data
                   );
                 }
               },
