551d86eeb4fcd2713764e7c8277b999cc0bc0b94
Keidra Floyd <keidrafloyd@gmail.com>
2024-04-27 14:01:48 -0400

    add intl package for the timestamp helper

--- lib/components/helper.dart
+++ lib/components/helper.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 String formatDate(Timestamp timestamp) {
   DateTime dateTime = timestamp.toDate();
@@ -10,4 +11,18 @@ String formatDate(Timestamp timestamp) {
   String formattedData = "$month/$day/$year";
 
   return formattedData;
+}
+
+String timeAgo(Timestamp timestamp) {
+  final now = DateTime.now();
+  final postDate = timestamp.toDate();
+  final difference = now.difference(postDate);
+
+  if (difference.inDays > 0) {
+    return '${difference.inDays} day${difference.inDays > 1 ? 's' : ''} ago';
+  } else if (difference.inHours > 0) {
+    return '${difference.inHours} hour${difference.inHours > 1 ? 's' : ''} ago';
+  } else {
+    return '${difference.inMinutes} minute${difference.inMinutes > 1 ? 's' : ''} ago';
+  }
 }
